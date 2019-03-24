dd3ffdd86e022377ef4f5b0f78e1c9e4dd90fa50
success jimoh <jimohsuccessdaodu@gmail.com>
2019-03-25 00:45:24 +0100

    added a section for total prices of item

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,9 +2,28 @@ let bar=document.querySelectorAll(".bar");
 let values=document.querySelectorAll(".value");
 let add=document.querySelectorAll(".add");
 let subtract=document.querySelectorAll(".subtract");
+let pvalue=document.querySelectorAll(".pvalue");
+let sumTotal=document.querySelector(".sumTotal");
+let total=0;
+
+function Ntotal(){
+    pvalue.forEach((value)=>{
+        total =total + Number(value.innerHTML);
+    
+   })
+
+}
+
+
+Ntotal();
+sumTotal.innerHTML=total+".00NGN";
 
 function deleteParent(){
     this.parentNode.remove();
+    pvalue=document.querySelectorAll(".pvalue");
+    total=0;
+    Ntotal();
+    sumTotal.innerHTML=total+".00NGN";
 }
 
 
@@ -12,6 +31,10 @@ function deleteParent(){
 function increase(e){
         let item = e.target.parentNode.previousSibling;
         ++item.textContent;
+        let price=e.target.parentNode.nextSibling.nextSibling.nextSibling.firstChild.innerHTML;
+        let quantityxValue=item.textContent*price;
+      console.log(quantityxValue);
+
     
 }
 
@@ -19,13 +42,20 @@ function decrease(e){
     let item = e.target.parentNode.previousSibling;
         if(item.textContent>1){
             --item.textContent;
+            let price=e.target.parentNode.nextSibling.nextSibling.nextSibling.firstChild.innerHTML;
+            let quantityxValue=item.textContent*price;
+            price.innerHTML=quantityxValue;
+            console.log(price);
+            console.log(quantityxValue);
         }
         else{
             item.textContent=0; 
+            quantityxValue=0;
                 
         }
   
 }
+
 
 
 bar.forEach((items)=>{
